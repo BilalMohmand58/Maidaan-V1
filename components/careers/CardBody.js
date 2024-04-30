dcfcca01daf33f5bdcf6b8118b8b6104330b491f
Bilal Ahmad <bilalahmadkhan1729@gmail.com>
2024-04-30 17:19:31 +0500

    Careers Post Cards Updated

--- components/careers/CardBody.js
+++ components/careers/CardBody.js
@@ -4,6 +4,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Link from "next/link";
+import { Button } from "react-bootstrap";
 
 function CardBody({ job }) {
   const { title, thumbnail, location, slug } = job?.fields;
@@ -12,22 +13,20 @@ function CardBody({ job }) {
       <Row className="justify-content-center">
         <Col sm={12} md={9}>
           <Card className="my-2">
-            <Card.Body className="d-flex align-items-center justify-content-between p-1 ">
-              <div className="d-flex align-items-center gap-2">
-                <img
-                  src={"https:" + thumbnail?.fields?.file?.url}
-                  alt="Your Image"
-                  className="mr-3 jobimg"
-                />
-                <h6 className="mb-0">{title}</h6>
-              </div>
-              <div className="d-flex align-items-center gap-3">
-                <Link
-                  href={"/careers/" + slug}
-                  className="text-decoration-none p-2"
-                >
-                  Apply Now
+            <Card.Body className="position-relative d-flex align-items-center justify-content-between p-1">
+              <div className="d-flex align-items-center">
+                <Link href={"/careers/" + slug}>
+                  {" "}
+                  <img
+                    src={"https:" + thumbnail?.fields?.file?.url}
+                    alt="Your Image"
+                    className="mr-3 jobimg"
+                  />
                 </Link>
+
+                {/* <Button className=" btn btn-lg btn-sm position-absolute top-50 end-0 translate-middle-y me-4 ">
+                  <Link href={"/careers/" + slug}>Apply Now</Link>
+                </Button> */}
               </div>
             </Card.Body>
           </Card>
